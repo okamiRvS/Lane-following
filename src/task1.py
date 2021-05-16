c424942362f53340dd33dd195161b2d1abc7f58f
usiusi <okamiRvS>
2021-05-16 10:37:23 -0700

    detect blue lines

--- src/task1.py
+++ src/task1.py
@@ -30,19 +30,44 @@ class Task1(ThymioController):
 
             # crop the image to make process much faster
             height, width, channels = cv_image.shape
-            crop_img = cv_image[height-200::,:,:]
+            crop_img = cv_image[height-200::,:]
+
+            pdb.set_trace()
+
+            # convert RGB blue to BGR blue
+            RGB_blue = [58,99,173]
+            BGR_blue = np.uint8([[[RGB_blue[2], RGB_blue[1], RGB_blue[0]]]])
+
+            # get HSV blue and a range 
+            HSV_blue = cv2.cvtColor(BGR_blue, cv2.COLOR_BGR2HSV)[0][0]
+            lower_blue = np.array([HSV_blue[0]-15, HSV_blue[1]-15, HSV_blue[2]-15])
+            upper_blue = np.array([HSV_blue[0]+15, HSV_blue[1]+15, HSV_blue[2]+15])
 
             # convert from RGB to HSV
             hsv = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
 
-            # get blue lines
-            
+            # apply a mask
+            mask = cv2.inRange(hsv, lower_blue, upper_blue)
+            res = cv2.bitwise_and(crop_img, crop_img, mask=mask)
 
-            pdb.set_trace()
+            # calculate centroid
+            m = cv2.moments(mask, False)
+            try:
+                cx, cy = m['m10'] / m['m00'], m['m01'] / m['m00']
+            except ZeroDivisionError: #if we don't detect anything
+                cx, cy = (100, width/2) #center of the image
+
+            # draw the centroid
+            cv2.circle(res, (int(cx), int(cy)), 10, (0,0,255), -1)
+
+
         except CvBridgeError as e:
             print(e)
         
-        cv2.imshow("Image window", crop_img)
+        cv2.imshow("Original", cv_image)
+        cv2.imshow("HSV", hsv)
+        cv2.imshow("Mask", mask)
+        cv2.imshow("Res", res)
         cv2.waitKey(0) # you must put in pause gazebo
 
     def run(self):
