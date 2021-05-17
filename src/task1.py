019f5846f7ef066007c87071d7769a2ad7e58a1f
usiusi <okamiRvS>
2021-05-17 04:04:32 -0700

    read camera to know if turn left or turn right

--- src/task1.py
+++ src/task1.py
@@ -30,9 +30,8 @@ class Task1(ThymioController):
 
             # crop the image to make process much faster
             height, width, channels = cv_image.shape
-            crop_img = cv_image[height-200::,:]
-
-            pdb.set_trace()
+            cropxSize = 100
+            crop_img = cv_image[height-cropxSize::,:]
 
             # convert RGB blue to BGR blue
             RGB_blue = [58,99,173]
@@ -48,27 +47,67 @@ class Task1(ThymioController):
 
             # apply a mask
             mask = cv2.inRange(hsv, lower_blue, upper_blue)
-            res = cv2.bitwise_and(crop_img, crop_img, mask=mask)
 
-            # calculate centroid
-            m = cv2.moments(mask, False)
+            kernel = np.ones((5, 5), np.uint8)
+            blur = cv2.blur(mask, (5,5))
+            erosion = cv2.erode(blur, kernel, iterations=1)
+            #dilation = cv2.dilate(erosion, kernel, iterations=1)
+
+            res = cv2.bitwise_and(crop_img, crop_img, mask=erosion)
+
+            # find contours
+            contours, hierarchy = cv2.findContours(erosion, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+            # draw contours
+            cv2.drawContours(res, contours[0], 0, (0, 255, 0), 5)
+
+            # calculate centroid from left
+            m = cv2.moments(contours[0], False)
             try:
-                cx, cy = m['m10'] / m['m00'], m['m01'] / m['m00']
+                cx1, cy1 = int(m['m10'] / m['m00']), int(m['m01'] / m['m00'])
             except ZeroDivisionError: #if we don't detect anything
-                cx, cy = (100, width/2) #center of the image
+                cx1, cy1 = (int(cropxSize/2), int(width/2)) #center of the image
 
-            # draw the centroid
-            cv2.circle(res, (int(cx), int(cy)), 10, (0,0,255), -1)
+            # draw the centroid of the left line
+            cv2.circle(res, (cx1, cy1), 5, (0,0,255), -1)
 
+            # draw contours
+            cv2.drawContours(res, contours[1], 0, (0, 255, 0), 5)
+
+            # calculate centroid from right
+            m = cv2.moments(contours[1], False)
+            try:
+                cx2, cy2 = int(m['m10'] / m['m00']), int(m['m01'] / m['m00'])
+            except ZeroDivisionError: #if we don't detect anything
+                cx2, cy2 = (int(cropxSize/2), width/2) #center of the image
+
+            # draw the centroid of the right line
+            cv2.circle(res, (cx2, cy2), 5, (0,0,255), -1)
+
+            # draw the center of the lines
+            centerx, centery = int((cx1+cx2)/2), int((cy1+cy2)/2)
+            cv2.circle(res, (centerx, centery), 10, (0,0,255), -1)
+
+            pdb.set_trace()
+
+            if int(width/2) - centerx > 25:
+                print("turn left")
+            elif int(width/2) - centerx < -25:
+                print("turn right")
 
         except CvBridgeError as e:
             print(e)
         
+        '''
+        cv2.imshow("Blur", blur)
+        cv2.imshow("Erosion", erosion)
+        #cv2.imshow("Dilation", dilation)
         cv2.imshow("Original", cv_image)
         cv2.imshow("HSV", hsv)
         cv2.imshow("Mask", mask)
-        cv2.imshow("Res", res)
-        cv2.waitKey(0) # you must put in pause gazebo
+        '''
+        #cv2.imshow("Res", res)
+        #cv2.waitKey(0) # you must put in pause gazebo
 
     def run(self):
         pass
