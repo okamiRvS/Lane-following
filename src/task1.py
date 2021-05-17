448d3847d041d048ab8cc50dfd8709e866d03587
usiusi <okamiRvS>
2021-05-17 13:34:40 -0700

    boost and more control

--- src/task1.py
+++ src/task1.py
@@ -15,6 +15,7 @@ class Task1(ThymioController):
 
         self.bridge_object = CvBridge()
 
+        # pitch camera: 0.076417
         # rosmsg show sensor_msgs/Image
         # rostopic echo -n1 /thymio10/camera/image_raw/height
         # rostopic echo -n1 /thymio10/camera/image_raw/width
@@ -69,51 +70,94 @@ class Task1(ThymioController):
                 res = cv2.bitwise_and(crop_img, crop_img, mask=erosion)
 
                 # find contours
-                contours, hierarchy = cv2.findContours(erosion, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+                contours, hierarchy = cv2.findContours(erosion, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-                # draw contours
-                cv2.drawContours(res, contours[0], 0, (0, 255, 0), 5)
+                if len(contours) == 0:
+                    print("I can't see anything, help me") #do the same action 
+                elif len(contours) == 1:
+                    # draw contours
+                    cv2.drawContours(res, contours[0], -1, (0, 255, 0), 3)
 
-                # calculate centroid from left
-                m = cv2.moments(contours[0], False)
-                try:
-                    cx1, cy1 = int(m['m10'] / m['m00']), int(m['m01'] / m['m00'])
-                except ZeroDivisionError: #if we don't detect anything
-                    cx1, cy1 = (int(cropxSize/2), int(width/2)) #center of the image
+                    points = np.zeros((len(contours[0]), 2), dtype=np.uint16)
+                    for idx, point in enumerate(contours[0]):
+                        points[idx] = point[0]
 
-                # draw the centroid of the left line
-                cv2.circle(res, (cx1, cy1), 5, (0,0,255), -1)
+                    # point with higher y coordinate
+                    orientationPoint = points[np.argsort(points[:,1])[0]]
 
-                # draw contours
-                cv2.drawContours(res, contours[1], 0, (0, 255, 0), 5)
+                    #draw the orientationPoint
+                    cv2.circle(res, (orientationPoint[0], orientationPoint[1]), 5, (0,200,255), -1)
 
-                # calculate centroid from right
-                m = cv2.moments(contours[1], False)
-                try:
-                    cx2, cy2 = int(m['m10'] / m['m00']), int(m['m01'] / m['m00'])
-                except ZeroDivisionError: #if we don't detect anything
-                    cx2, cy2 = (int(cropxSize/2), width/2) #center of the image
+                    # calculate centroid
+                    m = cv2.moments(contours[0], False)
+                    try:
+                        cx1, cy1 = int(m['m10'] / m['m00']), int(m['m01'] / m['m00'])
+                    except ZeroDivisionError: #if we don't detect anything
+                        cx1, cy1 = (int(cropxSize/2), int(width/2)) #center of the image
 
-                # draw the centroid of the right line
-                cv2.circle(res, (cx2, cy2), 5, (0,0,255), -1)
+                    # draw the centroid of the line
+                    cv2.circle(res, (cx1, cy1), 5, (0,0,255), -1)
 
-                # draw the center of the lines
-                centerx, centery = int((cx1+cx2)/2), int((cy1+cy2)/2)
-                cv2.circle(res, (centerx, centery), 10, (0,0,255), -1)
+                    # if we are at the end of the curve boost a little bit
+                    # let's see the amount of pixel of the curve where they are
+                    boost = 1
+                    if len(points) == len(points[points[:,0] < int(width/2)]):
+                        boost = 2
+
+                    if cx1 - orientationPoint[0] < 0:
+                        print("turn right")
+                        self.vel_msg.linear.x = .12
+                        self.vel_msg.angular.z = -.2 * boost
+                    elif cx1 - orientationPoint[0] >= 0:
+                        print("turn left")
+                        self.vel_msg.linear.x = .12
+                        self.vel_msg.angular.z = .2 * boost
+
+                elif len(contours) > 1:
+
+                    # draw contours left
+                    cv2.drawContours(res, contours[0], -1, (0, 255, 0), 3)
+
+                    # calculate centroid from left
+                    m = cv2.moments(contours[0], False)
+                    try:
+                        cx1, cy1 = int(m['m10'] / m['m00']), int(m['m01'] / m['m00'])
+                    except ZeroDivisionError: #if we don't detect anything
+                        cx1, cy1 = (int(cropxSize/2), int(width/2)) #center of the image
+
+                    # draw the centroid of the left line
+                    cv2.circle(res, (cx1, cy1), 5, (0,0,255), -1)
+
+                    # draw contours right
+                    cv2.drawContours(res, contours[1], -1, (0, 255, 0), 3)
+
+                    # calculate centroid from right
+                    m = cv2.moments(contours[1], False)
+                    try:
+                        cx2, cy2 = int(m['m10'] / m['m00']), int(m['m01'] / m['m00'])
+                    except ZeroDivisionError: #if we don't detect anything
+                        cx2, cy2 = (int(cropxSize/2), int(width/2)) #center of the image
+
+                    # draw the centroid of the right line
+                    cv2.circle(res, (cx2, cy2), 5, (0,0,255), -1)
+
+                    # draw the center of the lines
+                    centerx, centery = int((cx1+cx2)/2), int((cy1+cy2)/2)
+                    cv2.circle(res, (centerx, centery), 10, (0,0,255), -1)
 
 
-                if int(width/2) - centerx > 25:
-                    print("turn left")
-                    self.vel_msg.linear.x = .15
-                    self.vel_msg.angular.z = .1
-                elif int(width/2) - centerx < -25:
-                    print("turn right")
-                    self.vel_msg.linear.x = .15
-                    self.vel_msg.angular.z = -.1
-                else:
-                    print("I'm in the center")
-                    self.vel_msg.linear.x = .2
-                    self.vel_msg.angular.z = 0
+                    if int(width/2) - centerx > 10:
+                        print("turn left")
+                        self.vel_msg.linear.x = .15
+                        self.vel_msg.angular.z = .1
+                    elif int(width/2) - centerx < -10:
+                        print("turn right")
+                        self.vel_msg.linear.x = .15
+                        self.vel_msg.angular.z = -.1
+                    else:
+                        print("I'm in the center")
+                        self.vel_msg.linear.x = .2
+                        self.vel_msg.angular.z = 0
 
                 self.velocity_publisher.publish(self.vel_msg)
 
@@ -128,8 +172,8 @@ class Task1(ThymioController):
             cv2.imshow("HSV", hsv)
             cv2.imshow("Mask", mask)
             '''
-            #cv2.imshow("Res", res)
-            #cv2.waitKey(0) # you must put in pause gazebo
+            cv2.imshow("Res", res)
+            cv2.waitKey(1) # you must put in pause gazebo
         
             self.sleep()
 
